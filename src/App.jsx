33fe93553390d163a2e9540ee127a3ed170a6661
konrad <konradmocken@gmail.com>
2024-09-22 11:48:36 -0300

    Segunda Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import NavBar from './Componentes/NavBar/NavBar'
-import ItemListContainer from './Componentes/ItemListContainer/ItemListContainer'
-=======
 import React from 'react'
 import './App.css'
 import NavBar from './Componentes/NavBar/NavBar'
@@ -13,17 +5,12 @@ import ItemListContainer from './Componentes/ItemListContainer/ItemListContainer
 import ItemDetailContainer from './Componentes/ItemDetailContainer/ItemDetailContainer'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
->>>>>>> master
 
 function App() {
   
 
   return (
     <>
-<<<<<<< HEAD
-      <NavBar/>
-      <ItemListContainer greeting="Mis Productos"/>
-=======
       <BrowserRouter>
       <NavBar/>
 
@@ -38,7 +25,6 @@ function App() {
       
       
       
->>>>>>> master
     </>
   )
 }
